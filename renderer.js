eab223d3df45d33876ea9e66390db73f38c8b002
maxw2000 <maxvdwolf@gmail.com>
2021-01-06 15:33:58 +0100

    Discord RPC + Mute + Bug fixes

--- renderer.js
+++ renderer.js
@@ -3,7 +3,54 @@ var fs = require('fs');
 import { Brstm } from './lib/nikku/brstm/index.js';
 import { AudioPlayer } from './lib/nikku/audioPlayer.js';
 
-let audioPlayer = null;
+const DiscordRPC = require('discord-rpc');
+
+const clientId = '761185322192732200';
+const rpc = new DiscordRPC.Client({ transport: 'ipc' });
+
+
+var discord_details = "Idle.";
+var discord_file = "Idle.";
+var discord_time = new Date();
+var discord_filetype = "none";
+var discord_playstate = "play";
+
+
+async function setActivity() {
+  if (!rpc) {
+    return;
+  }
+
+  rpc.setActivity({
+    details: discord_details,
+    state: discord_file,
+    startTimestamp: discord_time,
+    largeImageKey: discord_filetype,
+    largeImageText: 'Playing a BRSTM file.',
+    smallImageKey: 'play',
+    smallImageText: 'Playing...',
+    instance: false,
+  });
+}
+rpc.on('ready', () => {
+  setActivity();
+
+  // activity can only be set every 15 seconds
+  setInterval(() => {
+    setActivity();
+  }, 15e3);
+});
+
+rpc.login({ clientId }).catch(console.error);
+
+
+
+
+
+
+
+
+let audioPlayer = new Audio();
 const elTime = document.getElementById('controls-time');
 const elPlayPause = document.getElementById('controls-play-pause');
 const elLoop = document.getElementById('controls-loop');
@@ -12,20 +59,33 @@ const elTimeCurrent = document.getElementById('controls-time-current');
 const elTimeAmount = document.getElementById('controls-time-amount');
 const elStreamSelect = document.getElementById('controls-stream-select');
 const elErrors = document.getElementById('errors');
+
+
+const buttonMuteUnmute = document.getElementById('controls-mute-unmute');
+
+
 const main = document.getElementById('main');
 const menu = document.getElementById('controls-menu');
+
+const videoPlayer = document.getElementById('video-player');
+const videoSource = document.getElementById('video-source');
+const audioPlayer_um = document.getElementById('audio-player');
+const audioSource = document.getElementById('audio-source');
+
+const screenLogo = document.getElementById('screen-logo');
 
 let currentTimeRenderAf = null;
 let shouldCurrentTimeRender = false;
 let isElTimeDragging = false;
 let streamStates = [true];
 let looped = false;
-let volume = 1;
 let hidemenu_timer = 3;
-let mediaType = 0; // 0 = nothing playing, 1 = brstm, 2 = video;
+let mediaType = 0; // 0 = nothing playing, 1 = nikku engine, 2 = default video engine, 3 = default audio engine;
+let muted = false;
 
 
-window.addEventListener('DOMContentLoaded', () => {
+
+document.addEventListener('DOMContentLoaded', () => {
 
 
 	// Timers
@@ -43,27 +103,26 @@ window.addEventListener('DOMContentLoaded', () => {
 		ev.preventDefault()
 	}
 	
-	window.addEventListener('mousemove', e => {
+	document.addEventListener('mousemove', e => {
 		hidemenu_timer = 3;
 		menu.style.display = "block";
 		menu.style.cursor = "default";
 	});
 	
 
-	window.addEventListener('wheel', e => {
+	document.addEventListener('wheel', e => {
 		hidemenu_timer = 3;
 		menu.style.display = "block";
 		menu.style.cursor = "default";
 		
 		if(Math.sign(e.deltaY) == -1) elVolume.value = parseInt(elVolume.value) + 5;
 		if(Math.sign(e.deltaY) == 1) elVolume.value = parseInt(elVolume.value) - 5;
-		console.log(elVolume.value);
 	
 		if(mediaType == 1){
 			audioPlayer.setVolume(elVolume.value / 100);
 			
 		} else if(mediaType == 2){
-			video.volume = elVolume.value / 100;
+			videoPlayer.volume = elVolume.value / 100;
 		}
 	});
 	
@@ -77,10 +136,32 @@ window.addEventListener('DOMContentLoaded', () => {
 			audioPlayer.setVolume(elVolume.value / 100);
 			
 		} else if(mediaType == 2){
-			video.volume = elVolume.value / 100;
+			videoPlayer.volume = elVolume.value / 100;
 		}
 	});
 	
+	
+	buttonMuteUnmute.addEventListener('click', (e) => {
+		e.preventDefault();
+		e.stopPropagation();
+		if(muted){
+			muted = false;
+			buttonMuteUnmute.src = "../../assets/png/mute.png";
+			if(mediaType == 1){
+				audioPlayer.setVolume(elVolume.value / 100);
+			} else if(mediaType == 2){
+				videoPlayer.volume = elVolume.value / 100;
+			}
+		} else {
+			muted = true;
+			buttonMuteUnmute.src = "../../assets/png/unmute.png";
+			if(mediaType == 1){
+				audioPlayer.setVolume(0);
+			} else if(mediaType == 2){
+				videoPlayer.volume = 0;
+			}
+		}
+	});
 	
 	elPlayPause.addEventListener('click', (e) => {
 		e.preventDefault();
@@ -98,25 +179,87 @@ window.addEventListener('DOMContentLoaded', () => {
 				enableStreamCheckboxes();
 			}
 		} else if(mediaType == 2){
-			if (video.paused) {
+			if (videoPlayer.paused) {
 				elPlayPause.src = "../../assets/png/pause.png";
-				video.play();
+				videoPlayer.play();
 			} else {
 				elPlayPause.src = "../../assets/png/play.png";
-				video.pause();
+				videoPlayer.pause();
+			}
+		} else if(mediaType == 3){ 
+			if (audioPlayer.paused) {
+				elPlayPause.src = "../../assets/png/pause.png";
+				audioPlayer.play();
+			} else {
+				elPlayPause.src = "../../assets/png/play.png";
+				audioPlayer.pause();
 			}
 		}
 	});
 	
 	
 	
+	videoPlayer.addEventListener('click', (e) => {
+		if (videoPlayer.paused) {
+			elPlayPause.src = "../../assets/png/pause.png";
+			videoPlayer.play();
+		} else {
+			elPlayPause.src = "../../assets/png/play.png";
+			videoPlayer.pause();
+
+		}
+	});
+	
+	
+	elLoop.addEventListener('click', (e) => {
+			e.preventDefault();
+			e.stopPropagation();
+			
+			if(looped){
+				looped = false;
+				elLoop.src = "../../assets/png/loop_off.png";
+			
+			} else {
+				looped = true;
+				elLoop.src = "../../assets/png/loop_on.png";
+			}
+			if(mediaType == 1){
+				audioPlayer.setLoop(looped);
+			}
+
+	});
+
+	let isElTimeDraggingTimeoutId = null;
+	elTime.addEventListener('input', (e) => {
+		e.preventDefault();
+		e.stopPropagation();
+
+			
+		isElTimeDragging = true;
+		if (isElTimeDraggingTimeoutId) {
+			clearTimeout(isElTimeDraggingTimeoutId);
+		}
+			
+		// Make sure targetTimeInMs is at most "xx.xx" (chop off the 0.00000000000001 floating point error)
+		isElTimeDraggingTimeoutId = setTimeout(() => {
+			isElTimeDragging = false;
+			if(mediaType == 1){
+				audioPlayer.seek(Math.round(parseFloat(e.target.value) * 1000 + 150) / 1000);
+			} else if(mediaType == 2){
+				videoPlayer.currentTime = videoPlayer.duration * e.target.value / 100;;
+				videoPlayer.play();
+			} else if(mediaType == 3){
+				audioPlayer.currentTime = audioPlayer.duration * e.target.value / 100;;
+				audioPlayer.play();
+			}
+			startRenderCurrentTime();
+			
+		}, 150);
+	});
 	
 	
 	
-	
-	
-	
-	window.addEventListener('keypress', e => {
+	window.addEventListener('keydown', e => {
 		
 		hidemenu_timer = 3;
 		menu.style.display = "block";
@@ -125,6 +268,7 @@ window.addEventListener('DOMContentLoaded', () => {
 		console.log(e.keyCode);
 		if(e.keyCode == 32){
 			if(mediaType == 1){
+				
 				if (audioPlayer.isPlaying) {
 					audioPlayer.pause();
 					stopRenderCurrentTime();
@@ -135,16 +279,82 @@ window.addEventListener('DOMContentLoaded', () => {
 					enableStreamCheckboxes();
 				}
 			} else if(mediaType == 2){
-				if (video.paused) {
+				
+				if (videoPlayer.paused) {
 					elPlayPause.src = "../../assets/png/pause.png";
-					video.play();
+					videoPlayer.play();
 				} else {
 					elPlayPause.src = "../../assets/png/play.png";
-					video.pause();
+					videoPlayer.pause();
+				}
+			} else  if(mediaType == 3){
+				
+				if (audioPlayer.paused) {
+					elPlayPause.src = "../../assets/png/pause.png";
+					audioPlayer.play();
+				} else {
+					elPlayPause.src = "../../assets/png/play.png";
+					audioPlayer.pause();
 				}
 			}
+		} else if(e.keyCode == 38){
+			
+			elVolume.value = parseInt(elVolume.value) + 5;
+	
+			if(mediaType == 1){
+				audioPlayer.setVolume(elVolume.value / 100);
+			} else if(mediaType == 2){
+				videoPlayer.volume = elVolume.value / 100;
+			}
+		} else if(e.keyCode == 40){
+			
+			elVolume.value = parseInt(elVolume.value) - 5;
+	
+			if(mediaType == 1){
+				audioPlayer.setVolume(elVolume.value / 100);
+			} else if(mediaType == 2){
+				videoPlayer.volume = elVolume.value / 100;
+			}
+		} else if(e.keyCode == 39){
+			
+			if(mediaType == 1){
+				audioPlayer.seek(audioPlayer.getCurrrentPlaybackTime() + 5);
+			} else if(mediaType == 2){
+				videoPlayer.currentTime = parseFloat(videoPlayer.currentTime) + 5;
+			}
+			
+			
+		} else if(e.keyCode == 37){
+			
+			if(mediaType == 1){
+				audioPlayer.seek(audioPlayer.getCurrrentPlaybackTime() - 5);
+			} else if(mediaType == 2){
+				videoPlayer.currentTime = parseFloat(videoPlayer.currentTime) - 5;
+			}
+			
+			
 		}
 		
+	});
+	
+	
+	
+	videoPlayer.addEventListener('ended', (e) => {
+		if(looped){
+			e.target.currentTime = 0;
+			e.target.play();
+		} else {
+			elPlayPause.src = "../../assets/png/replay.png";
+		}
+	});
+	
+	audioPlayer.addEventListener('ended', (e) => {
+		if(looped){
+			e.target.currentTime = 0;
+			e.target.play();
+		} else {
+			elPlayPause.src = "../../assets/png/replay.png";
+		}
 	});
 	
 	function hidemenu() {
@@ -156,7 +366,7 @@ window.addEventListener('DOMContentLoaded', () => {
 			hidemenu_timer--;
 		}
 	}
-	
+
 	
 	
 	if (remote.process.argv.length >= 2) {
@@ -170,7 +380,11 @@ window.addEventListener('DOMContentLoaded', () => {
 		}
 	}
 
-	function Play_File(path){
+	async function Play_File(path){
+		
+
+		Reset_Screen();
+		
 		
 		var file_name = path.split('\\').pop().split('/').pop();
 		var file_ext = file_name.split('.').pop().toLowerCase();
@@ -180,6 +394,12 @@ window.addEventListener('DOMContentLoaded', () => {
 			case "brstm":
 				mediaType = 1;
 				Play_BRSTM(path, file_name);
+				document.title = "UI-Player | Playing music - " + file_name;
+				discord_details = "Listening to Music";
+				discord_file = file_name;
+				discord_time = new Date();
+				discord_filetype = file_ext;
+				
 				break;
 			case "mp4":
 			case "avi":
@@ -188,95 +408,84 @@ window.addEventListener('DOMContentLoaded', () => {
 			case "webm":
 				mediaType = 2;
 				Play_Video(path, file_name);
+				document.title = "UI-Player | Playing video - " + file_name;
+				discord_details = "Watching a video";
+				discord_file = file_name;
+				discord_time = new Date();
+				discord_filetype = "mp4";
 				break;
 			case "mp3":
 			case "wav":
 			case "ogg":
-				alert("The only audio engine finished are for BRSTM files");
+			case "flac":
+				mediaType = 3;
+				playAudio(path);
+				document.title = "UI-Player | Playing music - " + file_name;
+				discord_details = "Listening to Music";
+				discord_file = file_name;
+				discord_time = new Date();
+				discord_filetype = "mp3";
 				break;
 		}
 	}
 
 
 
+
+
+
+	async function playAudio(path){
+		audioPlayer = new Audio(path);
+		
+		
+		
+		if(muted){
+			audioPlayer.volume = 0;
+		} else {
+			audioPlayer.volume = elVolume.value / 100;
+		}
+
+		audioPlayer.play();
+	}
+
 	async function Play_Video(path_full, file_name){
 		
-		if (audioPlayer) {
-			audioPlayer.destroy();
+		videoPlayer.style.display = "block";
+		screenLogo.style.display = "none";
+		videoSource.setAttribute('src', path_full);
+		videoPlayer.load();
+		
+		if(muted){
+			videoPlayer.volume = 0;
+		} else {
+			videoPlayer.volume = elVolume.value / 100;
 		}
 		
-		main.innerHTML = "<video id=\"video\"><source src=\"" + path_full + "\" type=\"video/mp4\"></video>";
-		const video = document.getElementById('video');
-		video.play();
+		for (var i = 0; i < videoPlayer.textTracks.length; i++) {
+			videoPlayer.textTracks[i].mode = 'showing';
+		}
+		
+		videoPlayer.play();
+		
+		
 		elPlayPause.src = "../../assets/png/pause.png";
-		let time = video.duration * 10;
+		let time = videoPlayer.duration * 10;
 		console.log(time);
 		elTimeAmount.textContent = formatTime(time);
 		
-		video.volume = 1;
-		video.muted = false;
 		
-		elVolume.addEventListener('input', (e) => {
-			e.preventDefault();
-			e.stopPropagation();
-
-			video.volume = e.target.value;
-		});
 		
-		elLoop.addEventListener('click', (e) => {
-			e.preventDefault();
-			e.stopPropagation();
-			
-			if(looped){
-				looped = false;
-				elLoop.src = "../../assets/png/loop_off.png";
-			
-			} else {
-				looped = true;
-				elLoop.src = "../../assets/png/loop_on.png";
-			}
-		});
-		
-		let isElTimeDraggingTimeoutId = null;
-		elTime.addEventListener('input', (e) => {
-			e.preventDefault();
-			e.stopPropagation();
-			
-			isElTimeDragging = true;
-			if (isElTimeDraggingTimeoutId) {
-				clearTimeout(isElTimeDraggingTimeoutId);
-			}
-			
-			// Make sure targetTimeInMs is at most "xx.xx" (chop off the 0.00000000000001 floating point error)
-			let targetTimeInS =  video.duration * e.target.value / 100;
-			console.log(targetTimeInS);
-			isElTimeDraggingTimeoutId = setTimeout(() => {
-				isElTimeDragging = false;
-				video.currentTime = targetTimeInS;
-				video.play();
-			}, 150);
-		});
-		
-		video.addEventListener('timeupdate', (e) => {
+		videoPlayer.addEventListener('timeupdate', (e) => {
 			renderCurrentTime()
 		});
 		
 		
-		video.addEventListener('click', (e) => {
-			if (video.paused) {
-				elPlayPause.src = "../../assets/png/pause.png";
-				video.play();
-			} else {
-				elPlayPause.src = "../../assets/png/play.png";
-				video.pause();
 
-			}
-		});
 		
 		function renderCurrentTime() {
-			const currentTime = video.currentTime;
+			const currentTime = videoPlayer.currentTime;
 			if (!isElTimeDragging) {
-				elTime.value = (video.currentTime / video.duration) * 100;
+				elTime.value = (videoPlayer.currentTime / videoPlayer.duration) * 100;
 			}
 			elTimeCurrent.textContent = formatTime(currentTime);
 
@@ -285,37 +494,36 @@ window.addEventListener('DOMContentLoaded', () => {
 			//}
 		}
 		
-		video.addEventListener('ended', (e) => {
-			if(looped){
-				e.target.currentTime = 0;
-				e.target.play();
-			} else {
-				elPlayPause.src = "../../assets/png/replay.png";
-			}
-		});
+
 	}
 	
 	
 	function Reset_Screen(){
-		main.innerHTML = "<img id=\"screen_logo\" src=\"../../assets/png/logo.png\"> ";
+		if(mediaType == 1){
+			if(audioPlayer) {
+				audioPlayer.destroy();
+			}
+		} else if(mediaType == 2) {
+			videoSource.setAttribute('src', "");
+			videoPlayer.load();
+			videoPlayer.style.display = "none";
+			screenLogo.style.display = "block";
+		} else if(mediaType == 3) {
+			
+		}
 	}
 	
 	
 	
 	//Play_BRSTM("D:/Documents/GitHub/UI-Player/aa.brstm", "aa.brstm");
 	async function Play_BRSTM(path_full, file_name){
-		Reset_Screen();
-		document.title = "UI-Player - Playing music - " + file_name;
+		
 		
 		fs.readFile(path_full, async (err, data) => {
 			try {
 				elErrors.textContent = '';
 
 				const brstm = new Brstm(data);
-
-				if (audioPlayer) {
-					audioPlayer.destroy();
-				}
 
 				audioPlayer = new AudioPlayer(brstm.metadata, {
 					onPlay: () => {
@@ -367,8 +575,13 @@ window.addEventListener('DOMContentLoaded', () => {
 				const amountTimeInS = brstm.metadata.totalSamples / brstm.metadata.sampleRate;
 				elTimeAmount.textContent = formatTime(amountTimeInS);
 				elTime.max = amountTimeInS;
-
-				audioPlayer.setVolume(elVolume.value / 100);
+				
+				if(muted){
+					audioPlayer.setVolume(0);
+				} else {
+					audioPlayer.setVolume(elVolume.value / 100);
+				}
+				
 				audioPlayer.setLoop(looped);
 				startRenderCurrentTime();
 			} catch (e) {
@@ -377,84 +590,6 @@ window.addEventListener('DOMContentLoaded', () => {
 			}
 		
 		});
-	
-		function renderCurrentTime() {
-			const currentTime = audioPlayer.getCurrrentPlaybackTime();
-			if (!isElTimeDragging) {
-				elTime.value = currentTime;
-			}
-			elTimeCurrent.textContent = formatTime(currentTime);
-
-			if (shouldCurrentTimeRender) {
-				currentTimeRenderAf = requestAnimationFrame(renderCurrentTime);
-			}
-		}
-
-		function startRenderCurrentTime() {
-			shouldCurrentTimeRender = true;
-			currentTimeRenderAf = requestAnimationFrame(renderCurrentTime);
-		}
-		
-		function stopRenderCurrentTime() {
-			if (currentTimeRenderAf) {
-				cancelAnimationFrame(currentTimeRenderAf);
-			}
-			shouldCurrentTimeRender = false;
-		}
-
-		function streamCheckedHandler(i) {
-			if (!audioPlayer) {
-				return;
-			}
-			streamStates[i] = !streamStates[i];
-			audioPlayer.setStreamStates(streamStates);
-		}
-
-
-		elLoop.addEventListener('click', (e) => {
-			e.preventDefault();
-			e.stopPropagation();
-			if (!audioPlayer) {
-				return;
-			}
-			
-			if(looped){
-				looped = false;
-				elLoop.src = "../../assets/png/loop_off.png";
-			
-			} else {
-				looped = true;
-				elLoop.src = "../../assets/png/loop_on.png";
-			}
-		
-			audioPlayer.setLoop(looped);
-		});
-
-		let isElTimeDraggingTimeoutId = null;
-		elTime.addEventListener('input', (e) => {
-			e.preventDefault();
-			e.stopPropagation();
-			if (!audioPlayer) {
-				return;
-			}
-			
-			isElTimeDragging = true;
-			if (isElTimeDraggingTimeoutId) {
-				clearTimeout(isElTimeDraggingTimeoutId);
-			}
-			
-			// Make sure targetTimeInMs is at most "xx.xx" (chop off the 0.00000000000001 floating point error)
-			let targetTimeInS = Math.round(parseFloat(e.target.value) * 1000 + 150) / 1000;
-			isElTimeDraggingTimeoutId = setTimeout(() => {
-				isElTimeDragging = false;
-				audioPlayer.seek(targetTimeInS);
-				startRenderCurrentTime();
-			}, 150);
-		});
-
-
-
-
 
 	}
 
@@ -472,17 +607,30 @@ window.addEventListener('DOMContentLoaded', () => {
 		}
 		return number;
 	}
-	function renderCurrentTime() {
-		const currentTime = audioPlayer.getCurrrentPlaybackTime();
-		if (!isElTimeDragging) {
-			elTime.value = currentTime;
-		}
-		elTimeCurrent.textContent = formatTime(currentTime);
+		function renderCurrentTime() {
+			
+			
+			
+			if(mediaType == 1){
+				const currentTime = audioPlayer.getCurrrentPlaybackTime();
+				if (!isElTimeDragging) {
+					elTime.value = currentTime;
+				}
+				elTimeCurrent.textContent = formatTime(currentTime);
 
-		if (shouldCurrentTimeRender) {
-			currentTimeRenderAf = requestAnimationFrame(renderCurrentTime);
+				if (shouldCurrentTimeRender) {
+					currentTimeRenderAf = requestAnimationFrame(renderCurrentTime);
+				}
+			}else if(mediaType == 2){
+			
+				const currentTime = videoPlayer.currentTime;
+				if (!isElTimeDragging) {
+					elTime.value = (videoPlayer.currentTime / videoPlayer.duration) * 100;
+				}
+				elTimeCurrent.textContent = formatTime(currentTime);
+			}
+
 		}
-	}
 
 	function startRenderCurrentTime() {
 		shouldCurrentTimeRender = true;
@@ -506,5 +654,13 @@ window.addEventListener('DOMContentLoaded', () => {
 		for (const child of elStreamSelect.childNodes) {
 			child.disabled = false;
 		}
+	}
+	
+	function streamCheckedHandler(i) {
+		if (!audioPlayer) {
+			return;
+		}
+		streamStates[i] = !streamStates[i];
+		audioPlayer.setStreamStates(streamStates);
 	}
 });
